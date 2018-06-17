912b1f9270cf40a3f84cf72de741f6dee7a67044
Apiggy <360625981@qq.com>
2018-06-17 15:43:57 +0800

    spark开发学习

--- src/main/java/com/self/learning/core/HdfsFile.java
+++ src/main/java/com/self/learning/core/HdfsFile.java
@@ -6,31 +6,29 @@ import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.api.java.function.Function;
 import org.apache.spark.api.java.function.Function2;
 
-public class LocalFile {
-
+public class HdfsFile {
     public static void main(String[] args) {
-        SparkConf conf = new SparkConf().setAppName("LocalFile").setMaster("local");
+        SparkConf conf = new SparkConf().setAppName("HdfsFile");
+        conf.set("spark.testing.memory", "1073741824");
         JavaSparkContext sc = new JavaSparkContext(conf);
-
-        JavaRDD<String> lines = sc.textFile("E:\\迅雷下载\\test_spark.txt");
+        JavaRDD<String> lines = sc.textFile("hdfs://spark1:9000/user/data/test_spark.txt");
         JavaRDD<Integer> lineLength = lines.map(new Function<String, Integer>() {
-            private static final long serialVersionUID = -4294166382487867787L;
+            private static final long serialVersionUID = -9216104884542425703L;
 
             @Override
             public Integer call(String line) throws Exception {
                 return line.length();
             }
         });
-        int count = lineLength.reduce(new Function2<Integer, Integer, Integer>() {
-            private static final long serialVersionUID = -5437722908076797183L;
+        Integer count = lineLength.reduce(new Function2<Integer, Integer, Integer>() {
+            private static final long serialVersionUID = -5159917128006407330L;
 
             @Override
             public Integer call(Integer v1, Integer v2) throws Exception {
                 return v1 + v2;
             }
         });
-        System.out.println("文件总字数是：" + count);
-
+        System.out.println("文件总字数为：" + count);
         sc.close();
     }
 }
